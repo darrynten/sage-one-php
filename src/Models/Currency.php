56a5997f05bff30bfd6ffd407da921e64540e72f
make-it-git <make.it.git@gmail.com>
2017-07-07 16:30:10 +0300

    Removed unrequired comments

--- src/Models/Currency.php
+++ src/Models/Currency.php
@@ -48,37 +48,6 @@ class Currency extends BaseModel
     protected $endpoint = 'Currency';
 
     /**
-     * Defines all possible fields.
-     *
-     * This maps to all the public properties you added above
-     *
-     * Used by the base class to decide what gets submitted in a save call,
-     * validation, etc
-     *
-     * All must include a type, whether or not it's nullable, and whether or
-     * not it's persistable.
-     *
-     * Documentation -> Model Conversion Rules:
-     *
-     * - Nullable is `true` if the word 'nullable' is in the 'type' column
-     * - Persistable is `true` if the word 'None.' is in the Additional Info column.
-     * - Type has the following rules
-     *   - `date` becomes "DateTime"
-     *   - `nullable` is removed, i.e. "nullable integer" is only "integer"
-     *   - Multiword linked terms are concatenated, eg:
-     *     - "Example Category" becomes "ExampleCategory"
-     *     - "Example Type" becomes "ExampleType"
-     *
-     * NB: Naming convention for keys is to lowercase the first character of the
-     * field returned by Sage (they use PascalCase and we use camelCase)
-     *
-     * `ID` is automatically converted to and from `id`
-     *
-     * Include a link with Details on writable properties for Example:
-     * https://accounting.sageone.co.za/api/1.1.2/Help/ResourceModel?modelName=Example
-     *
-     * TODO Remove this docblock
-     *
      * @var array $fields
      */
     protected $fields = [
@@ -105,20 +74,6 @@ class Currency extends BaseModel
     ];
 
     /**
-     * Features supported by the endpoint
-     *
-     * These features enable and disable certain calls from the base model
-     *
-     * It covers any call documented with these methods
-     *
-     * GET Model/Get
-     * GET Model/Get/{id}
-     * DELETE Model/Delete/{id}
-     * POST Model/Save
-     *
-     * You must enable the supported combination of the above 4 calls if they
-     * are present in the documentation
-     *
      * @var array $features
      */
     protected $features = [
@@ -127,18 +82,4 @@ class Currency extends BaseModel
         'save' => false,
         'delete' => false,
     ];
-
-    /**
-     * Any additional construction you may need to do
-     *
-     * @var array $config
-     */
-    public function __construct(array $config)
-    {
-        // Your code here
-        // Only include if you need to modify constructor
-
-        // This must always happen
-        parent::__construct($config);
-    }
 }
