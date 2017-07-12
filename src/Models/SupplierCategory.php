2a58866a8c84c877b8cab2b86f39c64689b56963
Igor <igorsergiichuk@gmail.com>
2017-07-12 22:35:02 +0300

    Add files via upload

---
+++ src/Models/SupplierCategory.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * SageOne Library
+ *
+ * @category Library
+ * @package  SageOne
+ * @author   Darryn Ten <darrynten@github.com>
+ * @license  MIT <https://github.com/darrynten/sage-one-php/blob/master/LICENSE>
+ * @link     https://github.com/darrynten/sage-one-php
+ */
+
+namespace DarrynTen\SageOne\Models;
+
+use DarrynTen\SageOne\BaseModel;
+
+/**
+ * Account Category
+ *
+ * Details on writable properties for Account:
+ * https://accounting.sageone.co.za/api/1.1.2/Help/ResourceModel?modelName=AccountCategory
+ */
+class SupplierCategory extends BaseModel
+{
+    /**
+     * The API Endpoint
+     *
+     * @var string $endpoint
+     */
+    protected $endpoint = 'SupplierCategory';
+
+    /**
+     * Defines all possible fields.
+     *
+     * Used by the base class to decide what gets submitted in a save call,
+     * validation, etc
+     *
+     * All must include a type, whether or not it's nullable, and whether or
+     * not it's readonly.
+     *
+     * NB: Naming convention for keys is to lowercase the first character of the
+     * field returned by Sage (they use PascalCase and we use camelCase)
+     *
+     * `ID` is automatically converted to `id`
+     *
+     * Details on writable properties for Account:
+     * https://accounting.sageone.co.za/api/1.1.2/Help/ResourceModel?modelName=AccountCategory
+     *
+     *
+     * @var array $fields
+     */
+    protected $fields = [
+        'id' => [
+            'type' => 'integer',
+            'nullable' => false,
+            'readonly' => false,
+            'required' => true,
+        ],
+        'description' => [
+            'type' => 'string',
+            'nullable' => false,
+            'readonly' => false,
+        ],
+        'modified' => [
+            'type' => 'DateTime',
+            'nullable' => true,
+            'readonly' => true,
+        ],
+        'created' => [
+            'type' => 'DateTime',
+            'nullable' => true,
+            'readonly' => true,
+        ],
+    ];
+
+    /**
+     * Features supported by the endpoint
+     *
+     * These features enable and disable certain calls from the base model
+     *
+     * @var array $features
+     */
+    protected $features = [
+        'all' => true,
+        'get' => true,
+        'save' => false,
+        'delete' => false,
+    ];
+}
