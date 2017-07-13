3dce97c38807f730494e94f22074c823cc80b44b
Igor <igorsergiichuk@gmail.com>
2017-07-13 15:20:55 +0300

    moved testAttributes() upper

--- tests/SageOne/Models/SupplierModelTest.php
+++ tests/SageOne/Models/SupplierModelTest.php
@@ -42,80 +42,6 @@ class SupplierModelTest extends BaseModelTest
         $this->verifyBadImport(Supplier::class, 'name');
     }
 
-    public function testInject()
-    {
-        $this->verifyInject(Supplier::class, function ($model) {
-            $this->assertEquals($model->id, 33);
-            $this->assertEquals($model->name, 'sample string 1');
-            $this->assertInstanceOf(SupplierCategory::class, $model->category);
-            $this->assertEquals($model->category->description, 'sample string 1');
-            $this->assertEquals($model->category->id, 2);
-            $this->assertEquals($model->category->modified->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->category->created->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->taxReference, 'sample string 2');
-            $this->assertEquals($model->contactName, 'sample string 3');
-            $this->assertEquals($model->telephone, 'sample string 4');
-            $this->assertEquals($model->fax, 'sample string 5');
-            $this->assertEquals($model->mobile, 'sample string 6');
-            $this->assertEquals($model->email, 'sample string 7');
-            $this->assertEquals($model->webAddress, 'sample string 8');
-            $this->assertTrue($model->active);
-            $this->assertEquals($model->balance, 10.0);
-            $this->assertEquals($model->creditLimit, 11.0);
-            $this->assertEquals($model->postalAddress01, 'sample string 12');
-            $this->assertEquals($model->postalAddress02, 'sample string 13');
-            $this->assertEquals($model->postalAddress03, 'sample string 14');
-            $this->assertEquals($model->postalAddress04, 'sample string 15');
-            $this->assertEquals($model->postalAddress05, 'sample string 16');
-            $this->assertEquals($model->deliveryAddress01, 'sample string 17');
-            $this->assertEquals($model->deliveryAddress02, 'sample string 18');
-            $this->assertEquals($model->deliveryAddress03, 'sample string 19');
-            $this->assertEquals($model->deliveryAddress04, 'sample string 20');
-            $this->assertEquals($model->deliveryAddress05, 'sample string 21');
-            $this->assertTrue($model->autoAllocateToOldestInvoice);
-            $this->assertEquals($model->textField1, 'sample string 22');
-            $this->assertEquals($model->textField2, 'sample string 23');
-            $this->assertEquals($model->textField3, 'sample string 24');
-            $this->assertEquals($model->numericField1, 1.0);
-            $this->assertEquals($model->numericField2, 1.0);
-            $this->assertEquals($model->numericField3, 1.0);
-            $this->assertTrue($model->yesNoField1);
-            $this->assertTrue($model->yesNoField2);
-            $this->assertTrue($model->yesNoField3);
-            $this->assertEquals($model->dateField1->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->dateField2->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->dateField3->format('Y-m-d'), '2017-07-04');
-            $this->assertInstanceOf(\DateTime::class, $model->modified);
-            $this->assertEquals($model->modified->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->modified->getTimezone()->getName(), 'UTC');
-            $this->assertInstanceOf(\DateTime::class, $model->created);
-            $this->assertEquals($model->created->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->created->getTimezone()->getName(), 'UTC');
-            $this->assertEquals($model->businessRegistrationNumber, 'sample string 29');
-            $this->assertEquals($model->RMCDApprovalNumber, 'sample string 30');
-            $this->assertEquals($model->taxStatusVerified->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->currencyId, 1);
-            $this->assertEquals($model->currencySymbol, 'sample string 31');
-            $this->assertTrue($model->hasActivity);
-            $this->assertEquals($model->defaultDiscountPercentage, 1.0);
-            $this->assertEquals($model->defaultTaxTypeId, 1);
-            $this->assertInstanceOf(TaxType::class, $model->defaultTaxType);
-            $this->assertEquals($model->defaultTaxType->id, 1);
-            $this->assertEquals($model->defaultTaxType->name, 'sample string 2');
-            $this->assertEquals($model->defaultTaxType->percentage, 3.1);
-            $this->assertTrue($model->defaultTaxType->isDefault);
-            $this->assertTrue($model->defaultTaxType->hasActivity);
-            $this->assertTrue($model->defaultTaxType->isManualTax);
-            $this->assertEquals($model->defaultTaxType->created->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->defaultTaxType->modified->format('Y-m-d'), '2017-07-04');
-            $this->assertEquals($model->dueDateMethodId, 1);
-            $this->assertEquals($model->dueDateMethodValue, 1);
-
-            $objArray = json_decode($model->toJson(), true);
-            $this->assertCount(49, $objArray);
-        });
-    }
-
     public function testAttributes()
     {
         $this->verifyAttributes(Supplier::class, [
@@ -365,6 +291,80 @@ class SupplierModelTest extends BaseModelTest
                 'readonly' => false,
             ]
         ]);
+    }
+
+    public function testInject()
+    {
+        $this->verifyInject(Supplier::class, function ($model) {
+            $this->assertEquals($model->id, 33);
+            $this->assertEquals($model->name, 'sample string 1');
+            $this->assertInstanceOf(SupplierCategory::class, $model->category);
+            $this->assertEquals($model->category->description, 'sample string 1');
+            $this->assertEquals($model->category->id, 2);
+            $this->assertEquals($model->category->modified->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->category->created->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->taxReference, 'sample string 2');
+            $this->assertEquals($model->contactName, 'sample string 3');
+            $this->assertEquals($model->telephone, 'sample string 4');
+            $this->assertEquals($model->fax, 'sample string 5');
+            $this->assertEquals($model->mobile, 'sample string 6');
+            $this->assertEquals($model->email, 'sample string 7');
+            $this->assertEquals($model->webAddress, 'sample string 8');
+            $this->assertTrue($model->active);
+            $this->assertEquals($model->balance, 10.0);
+            $this->assertEquals($model->creditLimit, 11.0);
+            $this->assertEquals($model->postalAddress01, 'sample string 12');
+            $this->assertEquals($model->postalAddress02, 'sample string 13');
+            $this->assertEquals($model->postalAddress03, 'sample string 14');
+            $this->assertEquals($model->postalAddress04, 'sample string 15');
+            $this->assertEquals($model->postalAddress05, 'sample string 16');
+            $this->assertEquals($model->deliveryAddress01, 'sample string 17');
+            $this->assertEquals($model->deliveryAddress02, 'sample string 18');
+            $this->assertEquals($model->deliveryAddress03, 'sample string 19');
+            $this->assertEquals($model->deliveryAddress04, 'sample string 20');
+            $this->assertEquals($model->deliveryAddress05, 'sample string 21');
+            $this->assertTrue($model->autoAllocateToOldestInvoice);
+            $this->assertEquals($model->textField1, 'sample string 22');
+            $this->assertEquals($model->textField2, 'sample string 23');
+            $this->assertEquals($model->textField3, 'sample string 24');
+            $this->assertEquals($model->numericField1, 1.0);
+            $this->assertEquals($model->numericField2, 1.0);
+            $this->assertEquals($model->numericField3, 1.0);
+            $this->assertTrue($model->yesNoField1);
+            $this->assertTrue($model->yesNoField2);
+            $this->assertTrue($model->yesNoField3);
+            $this->assertEquals($model->dateField1->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->dateField2->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->dateField3->format('Y-m-d'), '2017-07-04');
+            $this->assertInstanceOf(\DateTime::class, $model->modified);
+            $this->assertEquals($model->modified->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->modified->getTimezone()->getName(), 'UTC');
+            $this->assertInstanceOf(\DateTime::class, $model->created);
+            $this->assertEquals($model->created->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->created->getTimezone()->getName(), 'UTC');
+            $this->assertEquals($model->businessRegistrationNumber, 'sample string 29');
+            $this->assertEquals($model->RMCDApprovalNumber, 'sample string 30');
+            $this->assertEquals($model->taxStatusVerified->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->currencyId, 1);
+            $this->assertEquals($model->currencySymbol, 'sample string 31');
+            $this->assertTrue($model->hasActivity);
+            $this->assertEquals($model->defaultDiscountPercentage, 1.0);
+            $this->assertEquals($model->defaultTaxTypeId, 1);
+            $this->assertInstanceOf(TaxType::class, $model->defaultTaxType);
+            $this->assertEquals($model->defaultTaxType->id, 1);
+            $this->assertEquals($model->defaultTaxType->name, 'sample string 2');
+            $this->assertEquals($model->defaultTaxType->percentage, 3.1);
+            $this->assertTrue($model->defaultTaxType->isDefault);
+            $this->assertTrue($model->defaultTaxType->hasActivity);
+            $this->assertTrue($model->defaultTaxType->isManualTax);
+            $this->assertEquals($model->defaultTaxType->created->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->defaultTaxType->modified->format('Y-m-d'), '2017-07-04');
+            $this->assertEquals($model->dueDateMethodId, 1);
+            $this->assertEquals($model->dueDateMethodValue, 1);
+
+            $objArray = json_decode($model->toJson(), true);
+            $this->assertCount(49, $objArray);
+        });
     }
 
     public function testFeatures()
