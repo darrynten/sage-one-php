c82256a1eddc411f6322ab32558fd4f3d9724455
Igor <igorsergiichuk@gmail.com>
2017-07-13 15:33:56 +0300

    comment for Supplier category fixed

--- src/Models/SupplierCategory.php
+++ src/Models/SupplierCategory.php
@@ -16,7 +16,7 @@ use DarrynTen\SageOne\BaseModel;
 /**
  * Supplier Category
  *
- * Details on writable properties for Account:
+ * Details on writable properties for SupplierCategory:
  * hhttps://accounting.sageone.co.za/api/1.1.2/#bookmark_SupplierCategory
  */
 class SupplierCategory extends BaseModel
@@ -31,18 +31,7 @@ class SupplierCategory extends BaseModel
     /**
      * Defines all possible fields.
      *
-     * Used by the base class to decide what gets submitted in a save call,
-     * validation, etc
-     *
-     * All must include a type, whether or not it's nullable, and whether or
-     * not it's readonly.
-     *
-     * NB: Naming convention for keys is to lowercase the first character of the
-     * field returned by Sage (they use PascalCase and we use camelCase)
-     *
-     * `ID` is automatically converted to `id`
-     *
-     * Details on writable properties for Account:
+     * Details on writable properties for SupplierCategory:
      * https://accounting.sageone.co.za/api/1.1.2/#bookmark_SupplierCategory
      *
      * @var array $fields
