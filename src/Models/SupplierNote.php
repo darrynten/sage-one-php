6636bfaa4ea11228c043f4cc2e93e8b03c7dde04
Fergus <strfer001@myuct.ac.za>
2017-07-18 09:41:41 +0200

    SupplierNote mocks and model complete

---
+++ src/Models/SupplierNote.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * SageOne Library
+ *
+ * Official Method Documentation:
+ * https://accounting.sageone.co.za/api/1.1.2/Help/ResourceModel?modelName=CurrencyResponse
+ *
+ * @category Library
+ * @package  SageOne
+ * @author   Fergus Strangways-Dixon <fergusdixon@gmail.com>
+ * @license  MIT <https://github.com/darrynten/sage-one-php/blob/master/LICENSE>
+ * @link     https://github.com/darrynten/sage-one-php
+ */
+amespace DarrynTen\SageOne\Models;
+
+use DarrynTen\SageOne\BaseModel;
+
+class SupplierNote extends BaseModel
+{
+	/*
+     * The API Endpoint
+     *
+     * @var string $endpoint
+     */
+	protected $endpoint = 'SupplierNote';
+
+	/**
+	* @var array $fields
+	*/
+	protected $fields = [
+		'supplierId' => [
+			'type' => 'integer',
+            'nullable' => false,
+            'readonly' => false,
+		],
+		'userId' => [
+			'type' => 'integer',
+            'nullable' => true,
+            'readonly' => false,
+		],
+		'priority' => [
+			'type' => 'integer',
+            'nullable' => true,
+            'readonly' => false,
+		],
+		'noteCategoryId' => [
+			'type' => 'integer',
+            'nullable' => true,
+            'readonly' => false,
+		],
+		'notifyAssignee' => [
+			'type' => 'boolean',
+            'nullable' => true,
+            'readonly' => false,
+		],
+		'id' => [
+			'type' => 'integer',
+            'nullable' => false,
+            'readonly' => false,
+		],
+		'subject' => [
+			'type' => 'string',
+            'nullable' => false,
+            'readonly' => false,
+		],
+		'entryDate' => [
+			'type' => 'DateTime',
+            'nullable' => true,
+            'readonly' => false,
+		],
+		'actionDate' => [
+			'type' => 'DateTime',
+            'nullable' => false,
+            'readonly' => false,
+		],
+		'status' => [
+			'type' => 'boolean',
+            'nullable' => true,
+            'readonly' => false,
+		],
+		'note' => [
+			'type' => 'string',
+            'nullable' => false,
+            'readonly' => false,
+		],
+		'hasAttachments' => [
+			'type' => 'boolean',
+            'nullable' => true,
+            'readonly' => false,
+		],
+	];
+
+	/**
+     * @var array $features
+     */
+	protected $features = [
+        'all' => true,
+        'get' => true,
+        'save' => true,
+        'delete' => true,
+    ];
+}
