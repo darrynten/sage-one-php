e3c17ae727202e6d96cea7809157778069506049
Fergus <strfer001@myuct.ac.za>
2017-07-19 10:36:54 +0200

    Added AnalysisCategory model with mocks

---
+++ src/Models/AnalysisCategory.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * SageOne Library
+ *
+ * @category Library
+ * @package  SageOne
+ * @author   Fergus Strangways-Dixon <fergusdixon@github.com>
+ * @license  MIT <https://github.com/darrynten/sage-one-php/blob/master/LICENSE>
+ * @link     https://github.com/darrynten/sage-one-php
+ */
+
+namespace DarrynTen\SageOne\Models;
+
+use DarrynTen\SageOne\BaseModel;
+
+/**
+ * Analysis Category Model
+ *
+ * Details on writable properties for Account Note:
+ * https://accounting.sageone.co.za/api/1.1.2/Help/ResourceModel?modelName=AnalysisCategory
+ */
+class AnalysisCategory extends BaseModel
+{
+	/**
+     * The API Endpoint
+     *
+     * @var string $endpoint
+     */
+    protected $endpoint = 'AnalysisCategory';
+
+    /**
+     * @var array $fields
+     */
+    protected $fields = [
+        'id' => [
+            'type' => 'integer',
+            'nullable' => false,
+            'readonly' => false,
+        ],
+        'analysisTypeId' => [
+            'type' => 'integer',
+            'nullable' => false,
+            'readonly' => false,
+        ],
+        'description' => [
+            'type' => 'integer',
+            'nullable' => false,
+            'readonly' => false,
+        ],        
+    ];
+
+    /**
+     * @var array $features
+     */
+    protected $features = [
+        'all' => true,
+        'get' => true,
+        'save' => true,
+        'delete' => true,
+    ];
+
+    /**
+     * Confirms if Delete is Allowed.
+     *
+     * @return bool
+     */
+    public function allowDelete()
+    {
+        return $this->request->request('POST', $this->endpoint, 'AllowDelete');
+    }
+}
