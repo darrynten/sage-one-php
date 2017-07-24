90d54dee10ff159f81379a6c0ed38c875c33dc2e
Fergus <strfer001@myuct.ac.za>
2017-07-24 12:57:57 +0200

    Code cleanup

--- tests/SageOne/Exception/NotYetImplementedExceptionTest.php
+++ tests/SageOne/Exception/NotYetImplementedExceptionTest.php
@@ -2,15 +2,12 @@
 
 namespace DarrynTen\SageOne\Tests\SageOne;
 
-
-
 use DarrynTen\SageOne\Exception\NotYetImplementedException;
 
 class NotYetImplementedExceptionTest extends \PHPUnit_Framework_TestCase
 {
-    public function testMethodNotImplemented(){
-
-
+    public function testMethodNotImplemented()
+    {
         $this->expectException(NotYetImplementedException::class);
         $this->expectExceptionMessage('Error, "/path/to/method:here()" Method not yet implemented. This still needs to be added, please consider contributing to the project.');
         $this->expectExceptionCode(10301);
